d9bcfed78f10c34fcee559916275f5bbd12925d1
Владимир Ермошин <beepultimate@gmail.com>
2017-09-18 21:16:50 +0300

    asdfk

--- smo/smo/Buffer.h
+++ smo/smo/Buffer.h
@@ -9,12 +9,23 @@
 class Buffer {
 public:
 	Buffer() = delete;
-	Buffer(HoldStrategy& holdStrategy):
+	Buffer(const HoldStrategy& holdStrategy):
 	holdStrategy(holdStrategy){
 	}
-	void setStrategy(HoldStrategy holdStrategy) {
+
+	virtual void setStrategy(const HoldStrategy holdStrategy) final {
 		this->holdStrategy = holdStrategy;
 	}
+	virtual std::vector<Request>& getClaster() final{
+		return claster;
+	}
+
+	virtual void putElement(Request& request) = 0;
+	virtual Request& digElement() = 0;
+	virtual bool isFull() = 0;
+	virtual bool isEmpty() = 0;
+	virtual void processRequest(Request& request) = 0;
+	
 private:
 	std::vector<Request> claster;
 	HoldStrategy holdStrategy;
