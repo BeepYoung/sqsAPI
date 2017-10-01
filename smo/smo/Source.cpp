ca124ac95d05918066b0212cdd19e8c55878d9f6
Владимир Ермошин <beepultimate@gmail.com>
2017-10-01 20:58:45 +0300

    fssdghjnk

---
+++ smo/smo/Source.cpp
@@ -0,0 +1,11 @@
+﻿#include "Source.h"
+
+//τ = –1/λ · Ln(r), r - random(0,1);
+
+Request Source::generate() {
+	float lambda = 1;
+	int tau;
+	float randInt = (rand() % 99 + 1)/100;
+	tau = -1 / lambda*log(randInt);
+	
+}
--- smo/smo/Source.h
+++ smo/smo/Source.h
@@ -1,11 +1,22 @@
-#ifndef SOURCE_H
+﻿#ifndef SOURCE_H
 #define SOUTCE_H
+
+//1.Источник
+//1.1.Генерирует время появления заявки
+
+#include <cstdlib>
+#include <cmath>
 
 #include "Request.h"
 
 class Source {
 public:
-	virtual Request generate() = 0;
+	virtual Request generate();
+private:
+	int lambda;
 };
 
 #endif // !SOURCE_H
+
+
+//τ = –1/λ · Ln(r), r - random(0,1);
